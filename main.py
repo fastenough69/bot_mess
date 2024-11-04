2a0254961e914309ee0fb2b8d22db86e09671499
Lisa <user@MacBook-Pro--Lisa.local>
2024-11-04 13:34:55 +0300

    first steps with telethon

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from telethon import TelegramClient, events
+from selector import Selector
+
+api_id = Selector.api_id
+api_hash = Selector.api_hash
+chat_id = Selector.chat_id
+client = TelegramClient('you', api_id, api_hash)
+
+async def main():
+    await client.start(bot_token='7749696451:AAGZHskBCdXfCcOWIIi7_9sRHdUY0PN_Owo')
+    await client.send_message(chat_id, 'Привет')
+
+with client:
+    client.loop.run_until_complete(main())
+
+
+
+
+
 
---
+++ selector.py
@@ -0,0 +1,4 @@
+class Selector:
+    api_id = 23528482
+    api_hash = 'e133f63957b281bbd323a8ca4e49ca5a'
+    chat_id = 1482790150
